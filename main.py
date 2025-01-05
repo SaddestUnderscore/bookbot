ec981fe6166dd6d12108d69c08fb03f18063986b
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-01-06 00:18:27 +0300

    finish report logic

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
     word_count = get_words_count(text)
-    print(text)
-    print(f"total word count: {word_count}")
-    print(get_character_count(text))
+    chars_list = list(get_character_count(text).items())
+
+    print(f"--- Begin report of {path_to_file} ---")
+    print(f"{word_count} words found in the document\n")
+    sorted_chars_list = sorted(chars_list, key=lambda item: item[1], reverse=True)
+    for char, count in sorted_chars_list:
+        print(f"The '{char}' character was found {count} times") 
+    print("--- End report ---")
 
 def get_character_count(text): # okay i need to print all the code i learned into my brain holy
     chars = {}
