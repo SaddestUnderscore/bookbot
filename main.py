27ef0b05557a04a49114b7cc49b4e7562909b0a3
SaddestUnderscore <156387855+SaddestUnderscore@users.noreply.github.com>
2025-01-05 23:43:19 +0300

    simplify more code, with solution

--- main.py
+++ main.py
@@ -6,41 +6,15 @@ def main():
     print(f"total word count: {word_count}")
     print(get_character_count(text))
 
-def get_character_count(dict):
-    path_to_file = "books/frankenstein.txt"
-    character_count = {
-        "a": 0,
-        "b": 0,
-        "c": 0,
-        "d": 0,
-        "e": 0,
-        "f": 0,
-        "g": 0,
-        "h": 0,
-        "i": 0,
-        "j": 0,
-        "k": 0,
-        "l": 0,
-        "m": 0,
-        "n": 0,
-        "o": 0,
-        "p": 0,
-        "q": 0,
-        "r": 0,
-        "s": 0,
-        "t": 0,
-        "u": 0,
-        "v": 0,
-        "w": 0,
-        "x": 0,
-        "y": 0,
-        "z": 0
-    }
-    for string in get_book_text(path_to_file.lower()):
-        for character in string:
-            if character in character_count:
-                character_count[character] += 1
-    return character_count
+def get_character_count(text): # okay i need to print all the code i learned into my brain holy
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 def get_words_count(text):
     return len(text.split())
